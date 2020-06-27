363fe9d534508fe3efae9d9e942de0e33edb7967
nyssajayne <nyssajayne@gmail.com>
2020-06-26 20:33:24 -0400

    making sure all the files are there

--- src/App.js
+++ src/App.js
@@ -1,26 +1,109 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js'
+import Idea from './Idea.js'
+import Share from './Share.js'
+import About from './About.js'
+import Modal from './Modal.js'
+const FB = window.FB;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      likethis: this.getRandomIdea(this.props.likethis),
+      likethat: this.getRandomIdea(this.props.likethat)
+    }
+  }
+
+  /* Helper class to generate random idea*/
+  getRandomInt = (max) => {
+      return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  /* Generate random idea */
+  getRandomIdea = (idea) => {
+    var randomInt = this.getRandomInt(idea.length);
+
+    return idea[randomInt];
+  }
+
+  /* Set the random idea */
+  setRandomIdea = () => {
+    this.setState({
+      likethis: this.getRandomIdea(this.props.likethis),
+      likethat: this.getRandomIdea(this.props.likethat)
+    })
+  }
+
+  showModal = () => {
+    let body = document.querySelector('body');
+
+    body.classList.contains('modal-open') ? body.classList.remove('modal-open') : body.classList.add('modal-open');
+  }
+
+  goToLink = (url, target) => {
+    window.open(url, target);
+  }
+
+  componentDidMount() {
+    window.fbAsyncInit = function() {
+      window.FB.init({
+        appId      : '520425498126829',
+        status     : true,
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.5'
+      });
+    };
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  postToFeed(likethis, likethat){
+    var obj = {
+      method: 'feed',
+      link: 'http://itslikethisforthat.com', 
+      name: `I've got an idea! It's like ${likethis} for ${likethat}!`,
+      description: `Get your own idea at It's Like This for That.`
+    };
+
+    function callback(response){}
+      window.FB.ui(obj, callback);
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        
+        <Idea
+          likethis={this.state.likethis}
+          likethat={this.state.likethat} />
+
+        <Share 
+          setRandomIdea={this.setRandomIdea}
+          showModal={this.showModal} />
+
+        <About goToLink={this.goToLink} />
+
+        <Modal
+          showModal={this.showModal}
+          goToLink={this.goToLink}
+          postToFeed={this.postToFeed}
+          likethis={this.state.likethis}
+          likethat={this.state.likethat} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,87 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const likethis = [
+	"Uber",
+	"AirBnB",
+	"Ravelry",
+	"Facebook",
+	"Twitter",
+	"Instagram",
+	"Snapchat",
+	"Spotify",
+	"Yellow Pages",
+	"MySpace",
+	"Etsy",
+	"Craig&rsquo;s List",
+	"Gumtree",
+	"Costco",
+	"7-Eleven",
+	"Amazon",
+	"Wikipedia",
+	"a garage sale",
+	"the tooth fairy",
+	"Santa&rsquo;s sack",
+	"Google Maps",
+	"Kickstarter",
+	"a TV renovations show",
+	"Netscape Navigator",
+	"Tindr",
+	"BuzzFeed",
+	"The New York Times",
+	"Netflix",
+	"The App Store",
+	"Apple Pay",
+	"Foursquare",
+	"American Idol",
+	"Top Model",
+	"Die Hard"
+]
+
+const likethat = [
+	"memorabillia",
+	"poodles",
+	"dance routines",
+	"cheese toasties",
+	"chicken salt",
+	"flip phones",
+	"out of date prunes",
+	"unopened Christmas cards",
+	"recycled toner cartridges",
+	"elephant poo",
+	"Mario Kart",
+	"replacement kidneys",
+	"soviet era military surplus",
+	"buying elections",
+	"rats",
+	"out of date medicine",
+	"MiniDisc players",
+	"public nudity",
+	"false teeth",
+	"chewing with your mouth shut",
+	"rusty bottle caps",
+	"bad Picasso forgeries",
+	"beards",
+	"bears",
+	"unaffordable housing",
+	"goats cheese",
+	"trapped wind",
+	"listicles",
+	"unlicensed heart surgeons",
+	"off-brand baked beans",
+	"poetry that&rsquo;s meant to rhyme but really doesn&rsquo;t",
+	"ways to enjoy raw onions",
+	"drug smugglers",
+	"painfully hot curry",
+	"American foreign policy",
+	"art made out of cheese",
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+    	likethis={likethis}
+    	likethat={likethat} />
   </React.StrictMode>,
   document.getElementById('root')
 );
